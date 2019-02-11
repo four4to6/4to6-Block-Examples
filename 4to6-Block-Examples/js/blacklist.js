566d7add7b8e19491a3c6ce3beff46dcbc96a4b8
FOUR 4to6 <39036561+four4to6@users.noreply.github.com>
2019-02-11 18:11:37 +0900

    Add files via upload

---
+++ 4to6-Block-Examples/js/blacklist.js
@@ -0,0 +1,65 @@
+﻿wp.domReady( function() {
+/**
+ *****【 4to6 Block BLACK LIST 】
+ *
+ *****【 WARNING 】*************
+ *
+ * List blocks that are not supposed to be used
+ * This information is【 TOP SECRET 】.
+ * Remove the corresponding block from the list when using it.
+ * In the unlikely event that information is lost to the outside
+ * 【 4to6 Block Information agency 】
+ * will erase the data.
+ *
+ ****  Below, BLACK LIST **********/
+
+/**
+ * カテゴリー：一般ブロック *
+ */
+// 【 該当なし 】
+
+/**
+ * カテゴリー：フォーマット *
+ */
+wp.blocks.unregisterBlockType( 'core/preformatted' );     // 整形済み
+wp.blocks.unregisterBlockType( 'core/pullquote' );        // プルクオート
+wp.blocks.unregisterBlockType( 'core/verse' );            // 詩
+
+/**
+ * カテゴリー：レイアウト要素 *
+ */
+// 【 該当なし 】
+
+/**
+ * カテゴリー：ウィジェット *
+ */
+wp.blocks.unregisterBlockType( 'core/archives' );          // アーカイブ
+wp.blocks.unregisterBlockType( 'core/categories' );        // カテゴリー
+wp.blocks.unregisterBlockType( 'core/latest-comments' );   // 最新のコメント
+
+/**
+ * カテゴリー：埋め込み *
+ */
+wp.blocks.unregisterBlockType( 'core-embed/soundcloud' );   // SoundCloud
+wp.blocks.unregisterBlockType( 'core-embed/spotify' );      // Spotify
+wp.blocks.unregisterBlockType( 'core-embed/animoto' );      // Animoto
+wp.blocks.unregisterBlockType( 'core-embed/cloudup' );      // Cloudup
+wp.blocks.unregisterBlockType( 'core-embed/collegehumor' ); // CollegeHumor
+wp.blocks.unregisterBlockType( 'core-embed/funnyordie' );   // Funny or Die
+wp.blocks.unregisterBlockType( 'core-embed/imgur' );        // Imgur
+wp.blocks.unregisterBlockType( 'core-embed/issuu' );        // Issuu
+wp.blocks.unregisterBlockType( 'core-embed/kickstarter' );  // Kickstarter
+wp.blocks.unregisterBlockType( 'core-embed/meetup-com' );   // Meetup.com
+wp.blocks.unregisterBlockType( 'core-embed/photobucket' );  // Photobucket
+wp.blocks.unregisterBlockType( 'core-embed/polldaddy' );    // Polldaddy
+wp.blocks.unregisterBlockType( 'core-embed/reverbnation' ); // ReverbNation
+wp.blocks.unregisterBlockType( 'core-embed/screencast' );   // Screencast
+wp.blocks.unregisterBlockType( 'core-embed/scribd' );       // Scribd
+wp.blocks.unregisterBlockType( 'core-embed/slideshare' );   // Slideshare
+wp.blocks.unregisterBlockType( 'core-embed/smugmug' );      // SmugMug
+wp.blocks.unregisterBlockType( 'core-embed/speaker-deck' ); // Speaker Deck
+wp.blocks.unregisterBlockType( 'core-embed/ted' );          // TED
+wp.blocks.unregisterBlockType( 'core-embed/tumblr' );       // Tumblr
+wp.blocks.unregisterBlockType( 'core-embed/videopress' );   // VideoPress
+wp.blocks.unregisterBlockType( 'core-embed/wordpress-tv' ); // WordPress.tv
+} );
---
+++ 4to6-Block-Examples/js/style.js
@@ -0,0 +1,21 @@
+﻿jQuery(document).ready(function($) {
+  wp.blocks.registerBlockStyle("core/paragraph", {
+    name: "default",
+    label: "標準",
+  });
+ 
+  wp.blocks.registerBlockStyle("core/paragraph", {
+    name: "dashborder",
+    label: "枠あり（点線）"
+  });
+
+  wp.blocks.registerBlockStyle("core/paragraph", {
+    name: "bg-red",
+    label: "背景色（黒板風）"
+  });
+
+  wp.blocks.registerBlockStyle("core/paragraph", {
+    name: "point",
+    label: "ポイント"
+  });
+});
